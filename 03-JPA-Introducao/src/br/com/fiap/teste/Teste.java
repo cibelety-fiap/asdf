81010745836b9032e5b7bc03f180bb99a2387ff3
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-15 09:35:38 -0200

    final aula 1502

---
+++ 03-JPA-Introducao/src/br/com/fiap/teste/Teste.java
@@ -0,0 +1,25 @@
+package br.com.fiap.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		//Criar um gerenciador de entidades:
+		
+		//Primeiro cria a fábrica:
+		EntityManagerFactory fabrica =
+				Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
+		
+		//Depois, a fabrica cria os Entities manager
+		EntityManager em = fabrica.createEntityManager();
+		
+		
+		
+		
+		fabrica.close();
+	}
+	
+}
