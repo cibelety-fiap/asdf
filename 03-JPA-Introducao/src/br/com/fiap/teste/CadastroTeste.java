5fb069f130cca1c3933cd502b14885a286506ddf
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-18 08:11:48 -0300

    teste

--- 03-JPA-Introducao/src/br/com/fiap/teste/CadastroTeste.java
+++ 03-JPA-Introducao/src/br/com/fiap/teste/CadastroTeste.java
@@ -1,10 +1,16 @@
 package br.com.fiap.teste;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Teste {
+import br.com.fiap.entity.Cliente;
+import br.com.fiap.entity.Genero;
+
+public class CadastroTeste {
 
 	public static void main(String[] args) {
 		//Criar um gerenciador de entidades:
@@ -15,6 +21,9 @@ public class Teste {
 		
 		//Depois, a fabrica cria os Entities manager
 		EntityManager em = fabrica.createEntityManager();
+		
+		//Instanciar o cliente
+		Cliente cliente = new Cliente("Marcos", new GregorianCalendar(2000, Calendar.FEBRUARY, 2), "123.123.123-99", Genero.MASCULINO, null, true);
 		
 		
 		fabrica.close();
